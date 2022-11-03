6bb865e5bcf5485917bc01bcee657c6d26cb315e
Evan <eedumas@gmail.com>
2022-11-03 08:31:55 -0700

    Tried a new solution and caused a memory leak, wild

--- script.js
+++ script.js
@@ -123,8 +123,74 @@ const game = (() => {
         // iterate through edges
         console.log("checking for win");
 
-        
+        // try next making a checkRow and checkCol function
 
+        // you pass it the row or col, and it returns true if the whole column
+        // then you can run the checkRow in a for loop, and have it pass up till length-1
+        // could even make a checkDiag function eh? no needed loop for that one
+
+        const checkRow = (grid,row) => {
+            let temp = grid[row][0];
+
+            if(temp === ""){
+                return ""; // escape out if first item is blank;
+            }
+
+            for(i = 0; i < (grid.length - 1); i++){
+                
+                if(temp === grid[row][i+1]){ // is it equal to the next one ?
+
+                    if(i === (grid.length - 1)){ // win condition at last space
+                        return temp; // elegant, as this is the winner!
+                    }
+                    temp = grid[row][i+1];
+                    continue;
+                }
+                else{
+
+                    return ""; //returns false if no match
+                }
+            }
+        }
+
+        const checkCol = (grid,col) => {
+            let temp = grid[0][col];
+
+            if(temp === ""){
+                return ""; // escape out if first item is blank;
+            }
+
+            for(i = 0; i < (grid.length - 1); i++){
+                
+                if(temp === grid[i+1][col]){ // is it equal to the next one ?
+
+                    if(i === (grid.length - 1)){ // win condition at last space
+                        return temp; // elegant, as this is the winner!
+                    }
+                    temp = grid[i+1][col];
+                    continue;
+                }
+
+                else{
+                    return ""; //returns false if no match
+                }
+            }
+        }
+
+        const checkDiag = (grid) => {
+            // write this laster (later/last)
+        }
+
+        for(i = 0; i < grid.length; i++){
+            if(checkRow(grid,i)){
+                win = true;
+                let winner = checkRow(grid,i);/// hmm, this is tricky, can't check col or row each time
+            }
+            if(checkCol(grid,i)){
+                win = true;
+                let winner = checkCol(grid,i);
+            }
+        }
     
     };
 
